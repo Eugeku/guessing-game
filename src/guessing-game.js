5cf99702c9d9bee7555e661fcd31a5c27be612b1
Eugeku <eugenekuzora@gmail.com>
2024-09-26 20:28:07 +0300

    feat: implement guess game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+        this.setRange(this.min, this.guess());
     }
 
     greater() {
-
+        this.setRange(this.guess(), this.max);
     }
 }
 
